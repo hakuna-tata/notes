afb16261499ba200ca11324b9d178c86d2e49e84
hakuna-tata <610578197@qq.com>
2020-05-18 16:06:26 +0800

    dfs bfs

--- docs/algorithm/search.js
+++ docs/algorithm/search.js
@@ -42,6 +42,8 @@ function floor(arr, target){
 
 // 二分搜索树（每个节点的键值大于左孩子小于右孩子）：递归
 const insertNode = Symbol('insertNode');
+const preOrderTraversal = Symbol('preOrderTraversal');
+const inOrderTraversal = Symbol('inOrderTraversal');
 
 class Node{
   constructor(value){
@@ -68,8 +70,100 @@ class BST{
     return root;
   }
 
+  [preOrderTraversal](root, arr){
+    if(root === null) return;
+    arr.push(root.value);
+    this[preOrderTraversal](root.left, arr);
+    this[preOrderTraversal](root.right, arr);
+  }
+
+  [inOrderTraversal](root, arr){
+    if(root === null) return;
+    this[inOrderTraversal](root.left, arr);
+    arr.push(root.value);
+    this[inOrderTraversal](root.right, arr);
+  }
+
   insert(value){
     let newNode = new Node(value);
     this.root = this[insertNode](this.root, newNode);
   }
+
+  // 递归前序遍历
+  preOrder(){
+    let arr = [];
+    this[preOrderTraversal](this.root, arr);
+    return arr;
+  }
+
+  // 非递归前序遍历
+  noRecursivePreOrder(){
+    let arr = [], stack = [], temp = this.root;
+    if(this.root === null) return [];
+    while(temp !== null || stack.length){
+      while(temp !== null){
+        stack.push(temp);
+        arr.push(temp.value);
+        temp = temp.left;
+      }
+      if(stack.length){
+        let node = stack.pop();
+        temp = node.right;
+      }
+    }
+    return arr;
+  }
+  
+  // 递归中序遍历
+  inOrder(){
+    let arr = [];
+    this[inOrderTraversal](this.root, arr);
+    return arr;
+  }
+  // 非递归中序遍历
+  noRecursiveInOrder(){
+    let arr = [], stack = [], temp = this.root;
+    if(this.root === null) return [];
+    while(temp !== null || stack.length){
+      while(temp !== null){
+        stack.push(temp)
+        temp = temp.left
+      }
+      if(stack.length){
+        let node = stack.pop();
+        arr.push(node.value);
+        temp = node.right;
+      }
+    }
+    return arr;
+  }
+
+  // 非递归后序遍历
+  noRecursivePostOrder(){
+    let arr = [], stack = [], temp = this.root;
+    if(this.root === null) return [];
+    stack.push(this.root);
+    while(stack.length){
+      let node = stack.pop();
+      arr.push(node.value);
+      node.left && stack.push(node.left);
+      node.right && stack.push(node.right);
+    }
+    return arr.reverse();
+  }
+
+  // 层序遍历（广度优先）
+  levelOrder(){
+    let arr = [], stack = [], temp = this.root;
+    if(this.root === null) return [];
+    stack.push(this.root);
+    while(stack.length){
+      let node = stack.shift();
+      arr.push(node.value);
+      node.left && stack.push(node.left);
+      node.right && stack.push(node.right);
+    }
+    return arr;
+  }
+
 }
